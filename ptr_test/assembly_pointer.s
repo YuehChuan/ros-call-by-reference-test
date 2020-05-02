fb154972ce88dfc42ee928f9012ef44a07a7c619
YuehChuan <schwarmcyc@hotmail.com>
2020-05-03 02:25:03 +0800

    add reference and pointer assembly

---
+++ ptr_test/assembly_pointer.s
@@ -0,0 +1,22 @@
+	.file	"assembly_pointer.cpp"
+	.text
+	.globl	_Z1fPi
+	.type	_Z1fPi, @function
+_Z1fPi:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_Z1fPi, .-_Z1fPi
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
---
+++ ptr_test/assembly_reference.s
@@ -0,0 +1,23 @@
+	.file	"assembly_reference.cpp"
+	.text
+	.globl	_Z1fRi
+	.type	_Z1fRi, @function
+_Z1fRi:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movl	(%rax), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_Z1fRi, .-_Z1fRi
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits

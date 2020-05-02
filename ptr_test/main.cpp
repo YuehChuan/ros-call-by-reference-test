968541cc9d404a32ccc0de914967def6381b1e4b
YuehChuan <schwarmcyc@hotmail.com>
2020-05-03 01:51:53 +0800

    add example

--- ptr_test/main.cpp
+++ ptr_test/main.cpp
@@ -3,6 +3,7 @@
 
 //compile: g++ -std=c++11 main.c -o main
 //void foo( const std::shared_ptr<int>& p) 10 times faster
+
 void foo( const std::shared_ptr<int> p)
 {
         static int x =0;
@@ -13,11 +14,12 @@ int main(void)
 {
         auto p =std::make_shared<int>();
         auto start = clock();
-        for(int i = 0; i < 500000000; i++)
-        {
+        *p = 3;
+        for(int i = 0; i < 100000; i++)
+        {       
                 foo(p);
         }
         std::cout << " Took " << (clock() -start)/CLOCKS_PER_SEC << " s " << std::endl;
-
+        printf("final p %d",*p);//1000 expect
         return 0;
 }
